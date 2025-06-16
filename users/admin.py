6be23d467f9bacb4f2c3602b7e74d645dea8b53d
Nasrudin <nasrudinbinuabubakar@gmail.com>
2025-06-16 11:56:45 +0300

    admin registery of models

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Instructor, Course, Review , Category
+from .models import (
+    Instructor, Course, Review, Category,
+    Section, Lecture, LectureResource,
+    Enrollment, CompletedLecture, Certificate
+)
 
 @admin.register(Instructor)
 class InstructorAdmin(admin.ModelAdmin):
@@ -19,3 +23,33 @@ class ReviewAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('course', 'order', 'title')
+    ordering = ('course', 'order')
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('section', 'order', 'title', 'lecture_type', 'is_preview')
+    ordering = ('section', 'order')
+
+@admin.register(LectureResource)
+class LectureResourceAdmin(admin.ModelAdmin):
+    list_display = ('lecture', 'order', 'title')
+    ordering = ('lecture', 'order')
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'enrolled_at', 'is_active')
+    ordering = ('course', 'enrolled_at')
+
+@admin.register(CompletedLecture)
+class CompletedLectureAdmin(admin.ModelAdmin):
+    list_display = ('enrollment', 'lecture', 'completed_at')
+    ordering = ('enrollment', 'completed_at')
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('enrollment', 'certificate_id', 'issued_at')
+    ordering = ('issued_at',)
